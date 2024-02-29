5044206a76b9302ae3b595c3a39dd265fe93fb7e
Cameron Roots <38054423+croots@users.noreply.github.com>
2024-02-29 15:32:16 -0600

    Restructuring to python package format #2

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ setup(
     version='0.1.0',
     packages=find_packages(), 
     install_requires=[
-        'Pandas', 
-        'Bio',
-        'Time'
+        'pandas', 
+        'biopython'
     ],
     entry_points={
         'console_scripts': [
             'run_short_sequence_finder=short_sequence_finder.short_seq_finder:main',
         ],
     },
-    author='Sreya Das',
-    author_email='sreyaldas@gmail.com',
-    description='This package detects short repeat sequences (6-15) in sequences ',
-    url='https://github.com/coolbears/genomescan/short_sequence_finder',
+    author='placeholder',
+    author_email='placeholder',
+    description='placeholder',
+    url='https://github.com/barricklab/efm-calculator2',
     classifiers=[
         'Programming Language :: Python :: 3',
     ],
