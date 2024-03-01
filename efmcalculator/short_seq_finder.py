98025b44bdbdeb59457fd0228e17955d5b94324f
Cameron Roots <38054423+croots@users.noreply.github.com>
2024-03-01 11:44:57 -0600

    Remove global variables

--- efmcalculator/short_seq_finder.py
+++ efmcalculator/short_seq_finder.py
@@ -1,9 +1,14 @@
 from Bio import SeqIO
+import logging
+import pkg_resources
 from Bio.Seq import Seq
 import pandas as pd
 import time
 import numpy
 import short_seq_scan as sss
+
+logger = logging.getLogger(__name__)
+logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 _start_time = time.time()
 _debug = False
@@ -11,36 +16,35 @@ MIN_SHORT_SEQ_LEN = 5
 MAX_SHORT_SEQ_LEN = 17
 
 
-def main(filename, output, isCircular, debug):
-    global _debug,_isCircular
-    _debug = debug
+def main(filename, output, isCircular):
     _isCircular = isCircular
     data = {'Sequence': [], 'Size': [], 'Distance': [], 'Start-Pos': [], 'End-Pos': []}
     df = pd.DataFrame(data)
-    seq_parser(filename, output, df)
-    if _debug:
-        print(df)
-        t_sec = round(time.time() - _start_time)
-        t_min, t_sec = divmod(t_sec, 60)
-        t_hour, t_min = divmod(t_min, 60)
-        print('\nTime taken: {}hour:{}min:{}sec'.format(t_hour, t_min, t_sec))
-       
+    seq_parser(filename, output, df, isCircular)
 
-def seq_parser(filename, output, df):
+    # Logging
+    logger.debug(df.head())
+    t_sec = round(time.time() - _start_time)
+    t_min, t_sec = divmod(t_sec, 60)
+    t_hour, t_min = divmod(t_min, 60)
+    logger.debug('\nTime taken: {}hour:{}min:{}sec'.format(t_hour, t_min, t_sec))
+
+
+def seq_parser(filename, output, df, isCircular):
     sequences = SeqIO.parse(filename, 'fasta')
     for record in sequences:
         #seq_len = len(record.seq)
         # adds first 20 bp to end
-        if _isCircular == True:
+        if isCircular:
             record = record + record[0:20]
         #Strips of new line special character
         seq = record.seq.strip("\n")
         seq_len=len(seq)
-        build_sub_seq_from_seq(seq, df, seq_len)
+        build_sub_seq_from_seq(seq, df, seq_len, isCircular)
     df.sort_values(['Size'], ascending=[True]).to_csv(output, index=False)
 
 
-def build_sub_seq_from_seq(seq, df, seq_len):
+def build_sub_seq_from_seq(seq, df, seq_len, isCircular):
     global visited_sequences
     visited_sequences = set()
     for i, letter in enumerate(seq):
@@ -48,7 +52,7 @@ def build_sub_seq_from_seq(seq, df, seq_len):
             if len(seq[i:i + j]) > MIN_SHORT_SEQ_LEN:
                 sub_seq = seq[i:i + j]
                 rem_seq = seq[i + 1:]
-                find_short_seq(seq, sub_seq, df, seq_len)
+                find_short_seq(seq, sub_seq, df, seq_len, isCircular)
 
 
 """
@@ -64,7 +68,7 @@ def build_sub_seq_from_seq(seq, df, seq_len):
 """
 
 
-def find_short_seq(seq, sub_seq, df, seq_len):
+def find_short_seq(seq, sub_seq, df, seq_len, isCircular):
     count = seq.count_overlap(sub_seq)
     if count > 1 and str(sub_seq) not in visited_sequences:
         df['Sequence'] = df['Sequence'].astype(str)
@@ -72,10 +76,9 @@ def find_short_seq(seq, sub_seq, df, seq_len):
         #df_filter = df[df['Sequence'].str.contains(str(sub_seq))]
         #if df_filter.empty:
             
-        for seq_attr in sss.scan_short_sequence(seq, sub_seq, seq_len,_isCircular, count):
-                if _debug:
-                    print("Sequence = {} : Size = {} : Distance = {} : Start-Pos = {} : End-Pos = {}".format(
-                        seq_attr.sub_seq, seq_attr.length, seq_attr.distance, seq_attr.start_pos, seq_attr.end_pos))
+        for seq_attr in sss.scan_short_sequence(seq, sub_seq, seq_len,isCircular, count):
+                logger.debug("Sequence = {} : Size = {} : Distance = {} : Start-Pos = {} : End-Pos = {}".format(
+                    seq_attr.sub_seq, seq_attr.length, seq_attr.distance, seq_attr.start_pos, seq_attr.end_pos))
                 df.loc[seq_attr.sub_seq + '~' + str(seq_attr.start_pos)] = [seq_attr.sub_seq, seq_attr.length,
                                                                             seq_attr.distance,
                                                                             seq_attr.start_pos, seq_attr.end_pos]
@@ -96,6 +99,6 @@ if __name__ == '__main__':
                         help='--debug/--no-debug prints debug information')
 
     args = parser.parse_args()
-    main(filename=args.input_file_name, output=args.output_csv_file_name, isCircular=args.isCircular,debug=args.debug)
+    main(filename=args.input_file_name, output=args.output_csv_file_name, isCircular=args.isCircular)
 
 # Example usage:
