c2caf551d6125e7854ea443fccf8bc983bad780a
kevin99111 <156483708+kevin99111@users.noreply.github.com>
2024-03-07 11:35:14 -0600

    tried to fix filter (still not working) #8
    code after everything we did in the morning meeting. Does NOT work

--- efmcalculator/SRS_filter.py
+++ efmcalculator/SRS_filter.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 
 def filter_redundant(df):
-    memory = [""]
     i = 0
-    while i < len(df):
+    length = len(df)
+    while i < length:
         # creates new df with only repeats that contain the sequence of the row
         similar = df[df['Sequence'].str.contains(df.loc[i, 'Sequence'])]
+        print(df.loc[i, 'Sequence'])
         # resets the index values of the similar df
         similar = similar.reset_index(drop=True)
         x = 0
@@ -18,20 +19,20 @@ def filter_redundant(df):
             if similar.loc[y, 'Sequence'] == df.loc[i, 'Sequence']:
                 if count < len(similar) and count == similar.loc[x + count, 'Occurrences']:
                     while x < count:
-                        print("drop", df.loc[i+x, 'Sequence'])
-                        df.drop([i+x], inplace=True)
+                        print("drop", df.loc[i, 'Sequence'])
+                        df.drop([i], inplace=True)
                         x += 1
+                        i += 1
                         deleted = True
-                    i -= count
             y += 1
-            df = df.reset_index(drop=True)
-        i += count
+
+        i += 1
     return df
 
 
 start = time.time()
 
-file = "G:\Other computers\My Computer\School\EvoStab\Computation\psl9Sreya_3.csv"
+file = r"G:\Other computers\My Computer\School\EvoStab\Computation\psl9Sreya_3.csv"
 
 df = pd.read_csv(file)
 
@@ -43,7 +44,7 @@ df.sort_values(by=['Size', 'Sequence'], ascending=[True, True], inplace=True)
 
 df = filter_redundant(df)
 print(df)
-df.to_csv("G:\Other computers\My Computer\School\EvoStab\Computation\psl9Sreya_3_filtered_3.csv", index=False)
+df.to_csv(r"G:\Other computers\My Computer\School\EvoStab\Computation\psl9Sreya_3_filter_5.csv", index=False)
 
 end = time.time()
 print("There are " + str(len(df)) + " repeated sequences")
