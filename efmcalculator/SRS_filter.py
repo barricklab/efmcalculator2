b9ecb0702cce63f36a6864d337ef15bd9a24b273
kevin99111 <156483708+kevin99111@users.noreply.github.com>
2024-03-07 09:06:35 -0600

    update filter code #8

--- efmcalculator/SRS_filter.py
+++ efmcalculator/SRS_filter.py
@@ -2,32 +2,25 @@ import time
 import pandas as pd
 
 
-def in_memory(input_list, search_string):
-    return any(item == search_string for item in input_list)
-
-
 def filter_redundant(df):
     memory = [""]
     i = 0
     while i < len(df):
         # creates new df with only repeats that contain the sequence of the row
-        if not in_memory(memory, df.loc[i, 'Sequence']):
-            memory.append(df.loc[i, 'Sequence'])
-            similar = df[df['Sequence'].str.contains(df.loc[i, 'Sequence'])]
-            # resets the index values of the similar df
-            similar = similar.reset_index(drop=True)
+        similar = df[df['Sequence'].str.contains(df.loc[i, 'Sequence'])]
+        # resets the index values of the similar df
+        similar = similar.reset_index(drop=True)
+        x = 0
+        count = df.loc[i, 'Occurrences']
+        if count < len(similar) and count == similar.loc[x + count, 'Occurrences']:
+            while x < count:
+                print("drop", df.loc[i+x, 'Sequence'])
+                df.drop([i+x], inplace=True)
+                x += 1
+            i -= count
 
-            # print(similar)
-            x = 0
-            count = df.loc[i, 'Occurrences']
-            if count < len(similar) and count == similar.loc[x + count, 'Occurrences']:
-                while x < count:
-                    print("drop", df.loc[i+x, 'Sequence'])
-                    df.drop([i+x], inplace=True)
-                    x += 1
-
-                df = df.reset_index(drop=True)
-        i += 1
+            df = df.reset_index(drop=True)
+        i += count
     return df
 
 
@@ -45,7 +38,7 @@ df.sort_values(by=['Size', 'Sequence'], ascending=[True, True], inplace=True)
 
 df = filter_redundant(df)
 print(df)
-df.to_csv("G:\Other computers\My Computer\School\EvoStab\Computation\psl9Sreya_3_filtered.csv", index=False)
+df.to_csv("G:\Other computers\My Computer\School\EvoStab\Computation\psl9Sreya_3_filtered_2.csv", index=False)
 
 end = time.time()
 print("There are " + str(len(df)) + " repeated sequences")
