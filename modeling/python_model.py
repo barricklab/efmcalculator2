cff5400df894a5b07d4da38ec9dc0be2326b7d38
sreya <sreyaldas@gmail.com>
2024-06-27 10:34:37 -0500

    pl to pd

--- modeling/python_model.py
+++ modeling/python_model.py
@@ -1,15 +1,16 @@
-import polars as pl
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from pygam import LinearGAM, s
 
 #load csv file
-data_of_interest = pl.read_csv('new.csv')
+data_of_interest = pd.read_csv('new.csv')
+print(data_of_interest.columns)
 
 
 #separating df
 X = data_of_interest[['RBP_Length', 'TBD_length']]
 y = data_of_interest['Mutation_Rate']
-weights = data_of_interest['Weight']
+weights = data_of_interest['weight']
 
 #gam model fit with cr splines and regularization b/c small data
 gam_model = LinearGAM(s(0, n_splines=4, spline_order=3) +  s(1, n_splines=4, spline_order=3)) 
@@ -17,7 +18,7 @@ gam_model.fit(X, y, weights=weights
               )
 
 #data not included
-new_data = pl.DataFrame({'RBP_Length': [6], 'TBD_length': [50]})
+new_data = pd.DataFrame({'RBP_Length': [6], 'TBD_length': [50]})
 
 
 #prediction site
