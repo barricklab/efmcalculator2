94a4bfc8e10589d934e128e1028eaf0d03f72500
kevin99111 <kevinyang260@gmail.com>
2024-07-04 14:19:26 -0500

    SSR filtering #8

--- efmcalculator/filtering.py
+++ efmcalculator/filtering.py
@@ -1,9 +1,9 @@
 import polars as pl
 
 
-def filter_ssrs(ssrs_dataframe):
-    ssrs_dataframe = (
-        ssrs_dataframe.lazy()
+def filter_ssrs(ssr_dataframe):
+    ssr_dataframe = (
+        ssr_dataframe.lazy()
         # Filter based on SSR definition
         .filter(
             (pl.col("repeat_len") >= 2)
@@ -13,8 +13,29 @@ def filter_ssrs(ssrs_dataframe):
     ).collect()
 
     # Keep SSR with lowest length
+    ssr_dataframe = (
+        ssr_dataframe.sort("repeat_len")
+        .group_by(pl.col("start"))
+        .head(1)
+    )
 
-    return ssrs_dataframe
+    # Delete SSR within other more important SSRs ("CACACA" will have SSR of "CA" and "AC")
+    ssr_dataframe = (
+    ssr_dataframe.sort(pl.col("start"))
+    .with_columns(
+        pl.col("repeat").shift(1).str.tail(1).alias("last_tail"),
+        pl.col("repeat").str.head(1).alias("head"),
+        pl.col("start").shift(1).alias("last_start"),
+        )
+    .filter(
+        (pl.col("last_start").is_null()) |
+        (pl.col("start") != pl.col("last_start") + 1) |
+        (pl.col("last_tail") != pl.col("head"))
+        )
+    .select(["repeat", "repeat_len", "start", "count"])
+    )
+
+    return ssr_dataframe
 
 
 def filter_rmds(rmd_dataframe):
