11e026a2a081fdaf84d48bcf225cce27213ec051
Cameron Roots <38054423+croots@users.noreply.github.com>
2025-05-12 14:01:53 -0500

    disable password

--- efmcalculator/webapp/webapp.py
+++ efmcalculator/webapp/webapp.py
@@ -73,27 +73,6 @@ def download_data(): # https://gist.github.com/snehankekre/2dcce9fb42b2f7e1742de
         height=0,
     )
 
-def check_password():
-    """Returns `True` if the user had the correct password."""
-    def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state.get("password", ""), st.secrets["password"]):
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]  # Don't store the password.
-        else:
-            st.session_state["password_correct"] = False
-
-    # Return True if the password is validated.
-    if st.session_state.get("password_correct", False):
-        return True
-
-    # Show input for password.
-    st.text_input(
-        "Password", type="password", on_change=password_entered, key="password"
-    )
-    if "password_correct" in st.session_state:
-        st.error("😕 Password incorrect")
-    return False
 
 def check_feats_look_circular(seq):
     """Checks to see if features look circular."""
@@ -115,10 +94,6 @@ def check_feats_look_circular(seq):
         return False
 
 def run_webapp():
-
-    if not check_password():
-        st.stop()  # Do not continue if check_password is not True.
-        pass
 
     st._config.set_option(f"theme.base", "light")
     st.set_page_config(
